d64fd3c5d58b69761a907da84bcd4b590dcc855e
gl-guccione <g.guccione3@gmail.com>
2020-08-26 13:59:28 +0200

    done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+// for console.log(1 => 100)
+
+// if number % 3 == 0 && number % 5 == 0     => console.log("FizzBuzz");
+
+// if number % 3 == 0                        => console.log("Fizz");
+
+// if number % 5 == 0                        => console.log("Buzz");
+
+// else                                      => console.log(number);
+
+
+for (var i = 0; i < 100; i++) {
+  var number = i + 1;
+  if (number % 3 == 0 && number % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 == 0) {
+    console.log("Fizz");
+  } else if (number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
+  }
+}
